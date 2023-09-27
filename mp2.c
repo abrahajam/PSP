fb5e40f14a30d1097c9a626da21e5db1fc152ff4
abrahajam <133852635+abrahajam@users.noreply.github.com>
2023-09-27 10:11:58 +0200

    Update mp2.c

--- mp2.c
+++ mp2.c
@@ -12,13 +12,13 @@ void main() {
     printf("ERROR !!! No se ha podido crear el proceso hijo...");
     exit(-1);
   }
-  if (JUAN == -1 ){ //Ha ocurrido un error
-    printf("ERROR !!! No se ha podido crear el proceso hijo...");
-    exit(-1);
-  }
    
   if (PEPE == 0 ){
   	JUAN = fork();
+    if (JUAN == -1 ){ //Ha ocurrido un error
+    printf("ERROR !!! No se ha podido crear el proceso hijo...");
+    exit(-1);
+    }
   	if(JUAN == 0){
   		printf("Soy el proceso NIETO ... pid = %d ppid= %d 	\n",getpid(),getppid());	
   	}
