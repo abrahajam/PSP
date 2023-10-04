9d18d44e6ff3416ebc8f9314123f9de90ef4b070
abrahajam <133852635+abrahajam@users.noreply.github.com>
2023-10-04 08:44:21 +0200

    Update mp4.c

--- mp4.c
+++ mp4.c
@@ -1,6 +1,3 @@
-//1. /*Al escribir por pantalla CCC el pid es 1000 y al entrar en el if y crear el fork no se sabria cual seria el siguiente en crearse podria ser primero la 
-ejecución de AAA o la de BBB*/
-//2. Salida: CCC AAA BBB. Si que podría generarse otra salida ya que el padre no espera al hijo
 //codigo modificado:
 #include <stdio.h>
 #include <stdlib.h>
