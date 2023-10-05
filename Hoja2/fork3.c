fc18abd6d4808f2ad84750aafda2fe6f1594d0a9
abrahajam <133852635+abrahajam@users.noreply.github.com>
2023-10-05 23:53:22 +0200

    Update fork3.c

--- Hoja2/fork3.c
+++ Hoja2/fork3.c
@@ -8,45 +8,48 @@ void main(){
 
 	pid_t pid,pid2,pid3,pid4,pid5,pid_abuelo,pid_abuelo2;
 	pid_abuelo2=getpid();
+	printf("Soy el proceso P1 pid = %d \n",getpid());
 	pid=fork();
 	
 	if(pid==0){
 	pid_abuelo=getpid();
 		pid2=fork();
 		if(pid2==0){
-		
+		printf("Soy el proceso P3 pid = %d pid abuelo = %d \n",getpid(),pid_abuelo2);
 			pid4=fork();
 			if(pid4==0){
 				printf("Soy el proceso P5 pid = %d pid abuelo = %d \n",getpid(),pid_abuelo);	
 			}
 			else{
 			wait(NULL);
-			printf("Soy el proceso P3 pid = %d pid abuelo = %d \n",getpid(),pid_abuelo2);	
+				
 			}
 		}
 		else{
+		printf("Soy el proceso P2 pid = %d \n",getpid());		
 		pid_abuelo=getpid();
 			pid3=fork();
 			wait(NULL);
 			if(pid3==0){
+			printf("Soy el proceso P4 pid = %d pid abuelo = %d \n",getpid(),pid_abuelo2);
 				pid5=fork();
 				if(pid5==0){
 					printf("Soy el proceso P6 pid = %d pid abuelo = %d \n",getpid(),pid_abuelo);		
 				}
 				else{
 				wait(NULL);
-					printf("Soy el proceso P4 pid = %d pid abuelo = %d \n",getpid(),pid_abuelo2);		
+							
 				}		
 			}
 			else{
 			wait(NULL);
-		printf("Soy el proceso P2 pid = %d \n",getpid());		
+		
 			}
 		}
 	}
 	else{
 		wait(NULL);
-		printf("Soy el proceso P1 pid = %d \n",getpid());	
+			
 	}
 	exit(0);
 }
