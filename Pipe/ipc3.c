79341d45be0ac10d08978fcd131ccb8aeec1d1d0
abrahajam <133852635+abrahajam@users.noreply.github.com>
2023-10-29 01:09:10 +0200

    Update ipc3.c

--- Pipe/ipc3.c
+++ Pipe/ipc3.c
@@ -6,14 +6,12 @@
 
 void main(){
 
-     int fd[2]; 
-     char buffer[2];
+     int fd[2],suma=0,resta=0,mut=0,div=0;
+    
+     char buffer[2],num1[2],num2[2];
      pid_t pid;
      time_t t;
      int numero1,numero2;
-
-
-
     
      // Creamos el pipe
      pipe(fd); 
@@ -26,18 +24,35 @@ void main(){
      {
      		close(fd[0]); // Cierra el descriptor de escritura
      		srand((unsigned) time(&t));
+     		
               	numero1 = rand()%50;
               	numero2 = rand()%50;
-               write (fd[1],numero1,sizeof(numero1));
-              	write (fd[1],numero2,sizeof(numero2));
+              	
+              	sprintf(num1,"%d",numero1);
+              	sprintf(num2,"%d",numero2);
+              	
+               write (fd[1],num1,sizeof(numero1));
+              	write (fd[1],num2,sizeof(numero2));
      }
      else
      
      {
-               close(fd[0]); // Cierra el descriptor de lectura
+     		wait(NULL);
+               close(fd[1]); // Cierra el descriptor de lectura
                
-              	
-     			wait(NULL);
+               read(fd[0],buffer,2);
+               numero1 = atoi(buffer);
+               read(fd[0],buffer,2);
+               numero2 = atoi(buffer);
+               
+               suma = numero1 + numero2;
+               printf("%d + %d = %d\n",numero1,numero2,suma);
+               resta = numero1 - numero2;
+               printf("%d - %d = %d\n",numero1,numero2,resta);
+               mut = numero1 * numero2;
+               printf("%d * %d = %d\n",numero1,numero2,mut);
+               div = numero1 / numero2;
+               printf("%d / %d = %d\n",numero1,numero2,div);
         
      }
 }
