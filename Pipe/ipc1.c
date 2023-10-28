d6b61bb054e5c427de75131f2fde22f82b605198
abrahajam <133852635+abrahajam@users.noreply.github.com>
2023-10-28 23:44:16 +0200

    Update ipc1.c

--- Pipe/ipc1.c
+++ Pipe/ipc1.c
@@ -3,17 +3,19 @@
 #include <unistd.h>
 #include <sys/wait.h> 
 #include <time.h>
+#include <string.h>
 
 
 void main(){
 
      int fd[2]; 
-     char buffer[40];
+     char buffer[24];
      time_t hora;
      char *fecha ;
 
      pid_t pid;
-
+     
+    
      // Creamos el pipe
      pipe(fd); 
      
@@ -25,17 +27,19 @@ void main(){
      {
                 close(fd[1]); // Cierra el descriptor de escritura
                 printf("Soy el proceso hijo con pid %d \n",getpid());
-                read(fd[0], buffer, 40);
+                read(fd[0], buffer, 24);
+                
                 printf("\t Mensaje leido del pipe: %s \n", buffer);
+     
      }
      
      else
      {
      		time(&hora);
-		     fecha = ctime(&hora) ;
+		fecha = ctime(&hora) ;
 
                 close(fd[0]); // Cierra el descriptor de lectura
-                write(fd[1], fecha, 40);  
+                write(fd[1], fecha, 24);  
                 wait(NULL);    
      }
      
