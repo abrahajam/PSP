ac5045edc95631bf1ca3c6b874d5086ac994819a
abrahajam <133852635+abrahajam@users.noreply.github.com>
2023-10-28 23:32:57 +0200

    Update ipc2.c

--- Pipe/ipc2.c
+++ Pipe/ipc2.c
@@ -6,6 +6,7 @@ void main(){
 
      int fd[2]; 
      char buffer[2];
+     
      pid_t pid;
     
      // Creamos el pipe
@@ -18,34 +19,40 @@ void main(){
      
      {
      		close(fd[1]); // Cierra el descriptor de escritura
-     		int suma=0,num=0;
-     		char num2;
+     		int suma=0,numero=0;
      		read(fd[0], buffer,2); 
-     		printf("\tNumero a sumar es: %s\n",buffer);
-        	while (1) {
-        		read(fd[0], buffer,2); 
-			suma+= (int )buffer;
-            	       if(buffer[0]=='+'){
+     		
+     		if(buffer[0]=='+'){
+     			printf("\tRecibido carácter +\n");//por si el primer número leido es el caracter +
+     		}
+     		else{
+     			numero = atoi(buffer);
+            	suma += numero;
+     			printf("\tNumero a sumar es: %s\n",buffer);
+     			while (1) {
+        			read(fd[0], buffer,2);
+            	 	if(buffer[0]=='+'){
             	       printf("\tRecibido carácter +\n");
             	       break;
-            	       }
-            	       printf("\tNumero a sumar es: %s\n",buffer);
-          	        	
-            	        
-        	}
-        	 printf("\tLa suma total es: %d\n",suma);
+            	    }
+            	    else{
+					 	numero = atoi(buffer);//convierte a int
+            	        suma += numero;
+            	       	printf("\tNumero a sumar es: %s\n",buffer);
+            	    }
+        		}
+     		}	
+     		printf("\tLa suma total es: %d\n",suma);
      }
      else
-     
      {
-                close(fd[0]); // Cierra el descriptor de lectura
-               write (fd[1],"12",2);
-               write (fd[1],"22",2);
-               write (fd[1],"32",2);
-               write (fd[1],"42",2);
-               write (fd[1],"+",2);
-              	
-     			wait(NULL);
-        
+     	close(fd[0]); // Cierra el descriptor de lectura
+     	
+     	write (fd[1],"12",2);
+        write (fd[1],"22",2);
+        write (fd[1],"32",2);
+        write (fd[1],"42",2);
+        write (fd[1],"+",2);
+        wait(NULL);
      }
 }
