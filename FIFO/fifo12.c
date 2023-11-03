a6483b5d3efa117e95de6f285bd610a3eb9a0712
abrahajam <133852635+abrahajam@users.noreply.github.com>
2023-11-03 11:34:10 +0100

    Update fifo12.c

--- FIFO/fifo12.c
+++ FIFO/fifo12.c
@@ -1,1 +1,38 @@
+//Proceso que lee del PIPE FIFO
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/stat.h> 
 
+
+
+int main(void)
+{
+   int fp,fp2,numero=0,numero2=0,p,factorial=1;  
+   char  buffer[2],factorial1[8];
+ 
+   p=mkfifo("FIFO1", S_IFIFO|0666);//permiso de lectura y escritura
+  
+   fp = open("FIFO1", 0); 
+   read(fp, buffer, 2); 
+
+   numero = atoi(buffer);
+   numero2 = numero;
+   
+   for(int i = 0; i < numero2;i++){
+   	factorial *= numero;
+  	numero--;
+   }
+   
+   sprintf(factorial1,"%d",factorial);
+   close(fp); 
+   /////////////////////////////////////////
+   fp2 = open("FIFO2", 1);  
+   
+   write(fp2,factorial1,sizeof(factorial1));
+   
+   close(fp2);
+  return(0);
+}
